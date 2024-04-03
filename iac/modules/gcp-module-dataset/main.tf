8102cc3aad03c1267c3c6fc2d18075d11a9e6668
X-I-A <develop@x-i-a.com>
2024-04-03 09:03:29 +0200

    feat: BQ Dataset development

--- iac/modules/gcp-module-dataset/main.tf
+++ iac/modules/gcp-module-dataset/main.tf
@@ -2,11 +2,11 @@ locals {
   contents         = yamldecode(file(var.content))
 }
 
-resource "google_bigquery_dataset" "sample_dataset" {
+resource "google_bigquery_dataset" "app_datasets" {
   dataset_id          = local.contents.dataset_id
   description         = local.contents.description
   friendly_name       = local.contents.friendly_name
   location            = local.contents.location
-  deletion_protection = local.contents.deletion_protection
+  delete_contents_on_destroy = local.contents.delete_contents_on_destroy
 }
 
